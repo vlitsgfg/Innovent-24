0d49e88b7ec833c78356c51e4df04aec5217e28b
a <a@mail.com>
2023-09-14 15:45:37 +0530

    first commit

---
+++ connection/connect.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost"; //server
+$username = "root"; //username
+$password = ""; //password
+$dbname = "homemade";  //database
+
+// Create connection
+$db = mysqli_connect($servername, $username, $password, $dbname); // connecting 
+// Check connection
+if (!$db) {       //checking connection to DB	
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
